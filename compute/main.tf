f8303ddf7f8b3226f6d06549792a36860666a6f7
Sameer Sonaikar <samisangel@gmail.com>
2023-07-09 09:36:09 -0400

    SSH key and CIDR route block change

--- compute/main.tf
+++ compute/main.tf
@@ -22,7 +22,7 @@ data "aws_ami" "ec2-image" {
 
 }
 
-data "aws_key_pair" "deployer" {
+resource "aws_key_pair" "deployer" {
   key_name   = "deployer-key"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCfvjpWm2ftg3tnOEcm+6W9fIG1JYZbxRSa0vwMWg73qKjR61d548nzaMtwJiuM2Qhs/4fOM9onoY/ZInaqL1shclEma9adi3EGlP/IHY41Wow/qTR+UET58Xbva8saYddJKB8pJZsGOkjj6itZmabZasmsRwuP16fKXAnln18bh2V54gRiwBND9xakjFcQ9T0/aczWqBmXra/2HXXK+d8hBC3ja26KyfkFIEjR1UZP4ftOY2FXiH2E7uOAGaKTPfneFDUG+Rd/n5T/KqhhazswFUnN/2rHVYBiReyW6Dmzkpdk642dNLQxoGTYaVKmI4Y13JKWB9ZfRCexJozx20FAJnc7+7Zw+jKodBubW91bsTzHGk/EwFofZyPC2tyFJnZWbChkmzce/5tgEI54zxb+tQ3AfAxnTL20HW7ZOh6+IhwXsYhSZYdqKLBQEaqEAO+hj/o4ugAMettLYwWHnIf+qbSsa2mGDJVpkyxN3W9OJN/OIDkcxnJcM6yZsUVcG6k= tarakant@US-3V33XD3"
 }
@@ -31,7 +31,7 @@ resource "aws_instance" "intuitive_instance" {
   count = var.vm-count
 
   ami             = data.aws_ami.ec2-image.id
-  key_name        = data.aws_key_pair.deployer.key_name
+  key_name        = aws_key_pair.deployer.key_name
   security_groups = [var.security_group_id]
   subnet_id       = var.subnet_id
   instance_type   = var.instance_type
